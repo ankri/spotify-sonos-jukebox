cf50a27d905de95d491f3749a65509ea13cbbab9
Andy Krings-Stern <github@ankri.de>
2022-02-18 16:01:39 +0100

    fix deprecation warning

--- cache/cache.ts
+++ cache/cache.ts
@@ -1,7 +1,9 @@
 import LRUCache from "lru-cache";
 
+// @ts-ignore ttl is not part of @types/lru-cache yet
+// https://github.com/DefinitelyTyped/DefinitelyTyped/pull/58853
 export const imageCache = new LRUCache<string, string>({
   max: 200,
   // TODO find suitable max age
-  maxAge: 1000 * 60 * 60 * 24,
+  ttl: 1000 * 60 * 60 * 24,
 });
