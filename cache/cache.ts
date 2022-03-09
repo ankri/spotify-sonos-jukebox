3b512b9a420f69ab5b240765040f8e0247429bd2
Andy Krings-Stern <github@ankri.de>
2022-03-09 23:33:47 +0100

    fixed upstream

--- cache/cache.ts
+++ cache/cache.ts
@@ -1,7 +1,5 @@
 import LRUCache from "lru-cache";
 
-// @ts-ignore ttl is not part of @types/lru-cache yet
-// https://github.com/DefinitelyTyped/DefinitelyTyped/pull/58853
 export const imageCache = new LRUCache<string, string>({
   max: 200,
   // TODO find suitable max age
